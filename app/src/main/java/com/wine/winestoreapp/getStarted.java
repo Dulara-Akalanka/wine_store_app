7f8c8255c97493075b9f5b640717d1b4b70ede05
dulara-akalanka <akalanka0910@gmail.com>
2024-11-11 21:41:11 +0530

    UI Design

--- app/src/main/java/com/wine/winestoreapp/getStarted.java
+++ app/src/main/java/com/wine/winestoreapp/getStarted.java
@@ -1,6 +1,9 @@
 package com.wine.winestoreapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class getStarted extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_get_started);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnGS = findViewById(R.id.btnGetStarted);
+        btnGS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getStarted.this,login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
