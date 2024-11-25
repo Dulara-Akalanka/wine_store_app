73859104c694a31cf8ddaf590817836da3e70611
dulara-akalanka <akalanka0910@gmail.com>
2024-11-25 22:15:30 +0530

    Item View

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,11 @@ dependencies {
     implementation(libs.annotation)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
